7122e8a817afd09c8777d54725ba23582458b06a
NgDuyPhong <NgDuyPhong@users.noreply.github.com>
2021-09-24 17:44:29 +0700

    commit fix

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ const crypto = require('crypto');
 
 // input start from
 let start = 0;
-let IRAD = 10.0;
 let count = 10;
 let stop = false;
 let dataRs = [];
 let sum = 0;
 let fileNameSuccess = "";
 let listHashRecruitmentID = [];
+let sumInWhile = 0;
 
 const listExclusionFlag = ["フォーク","ﾌｫｰｸ"];
 const header = [
@@ -38,16 +38,19 @@ function contentMethod(options, bukken_id, dateNow, lrad) {
 		let countItem = 0;
 		if(!error && response.statusCode == 200) {
 			const $ = cheerio.load(html);
-			setTimeout(function () {}, 500)
+			// setTimeout(function () {}, 500)
 			let data = [];
-			countItem = count = $('.pE8vnd.avtvi').length;
-			$('.pE8vnd.avtvi').each((index, el) => {
-				const jobName = $(el).find('.sH3zFd h2').text();
-				const postPerson = $(el).find('.nJlQNd.sMzDkb').text();
+			// console.log((html));
+			countItem = count = $('.PwjeAc').length;
+			$('.PwjeAc').each((index, el) => {
+				const jobName = $(el).find('.BjJfJf.PUpOsf').text();
+				const postPerson = $(el).find('.vNEEBe').text();
 				let salaryMin = "";
 				let salaryMax = "";
 				let jobType = "";
-				$(el).find('.ocResc.icFQAc .n1Mpqb').each((i,ele) => {
+				let provider = "";
+
+				$(el).find('.KKh3md.zfZM7e .n1Mpqb').each((i,ele) => {
 				if (i!=0){
 					let str = $(ele).find('.SuWscb').text();
 					if (listJobType.indexOf(str) != -1) {
@@ -61,16 +64,16 @@ function contentMethod(options, bukken_id, dateNow, lrad) {
 					}
 				}
 				});
-				const provider = $(el).find('.pMhGee.Co68jc.j0vryd').text().split(": ")[1];
+				provider = $(el).find('.Qk80Jf').text().split(": ")[1];
 				salaryMin = formatSalary(salaryMin);
 				salaryMax = formatSalary(salaryMax);
-				const recruitmentID = "0";
-				// create key
-				// const recruitmentID = provider + jobName + salaryMin + salaryMax + jobType;
-				// const hashRecruitmentID = crypto.createHash('md5').update(recruitmentID).digest('hex');
 
-				// if (listHashRecruitmentID.indexOf(hashRecruitmentID) === -1) {
-				// 	listHashRecruitmentID.push(hashRecruitmentID);
+				// create key
+				const recruitmentID = '1' + provider + '2' + jobName + '3' + salaryMin + '4' + salaryMax + '5' + jobType;
+				const hashRecruitmentID = crypto.createHash('md5').update(recruitmentID).digest('hex');
+				// const recruitmentID = "0"
+				if (listHashRecruitmentID.indexOf(hashRecruitmentID) === -1) {
+					listHashRecruitmentID.push(hashRecruitmentID);
 					let date = moment(dateNow).format('YYYY-MM-DD');
 					let exclusionFlag = "N";
 					if (jobName.indexOf(listExclusionFlag[0]) != -1 || jobName.indexOf(listExclusionFlag[1]) != -1) {
@@ -78,12 +81,12 @@ function contentMethod(options, bukken_id, dateNow, lrad) {
 					}
 					let lradStr = lrad === "" ? "2km" : "10km";
 					data.push({
-						recruitmentID, bukken_id, date, lradStr, exclusionFlag, postPerson, provider, jobName, salaryMin, salaryMax, jobType
+						hashRecruitmentID, bukken_id, date, lradStr, exclusionFlag, postPerson, provider, jobName, salaryMin, salaryMax, jobType
 					});
-				// } else {
-				// 	// sum item of file output-- if recruitmentID exists
-				// 	countItem > 0 ? countItem-- : (countItem = 0);
-				// }
+				} else {
+					// sum item of file output-- if recruitmentID exists
+					countItem > 0 ? countItem-- : (countItem = 0);
+				}
 				
 			});
 			dataRs.push(...data);
@@ -91,10 +94,13 @@ function contentMethod(options, bukken_id, dateNow, lrad) {
 		else {
 			console.log(error);
 		}
-
-		if (count != 0) {
-			sum += count;
+		sumInWhile += count;
+		console.log(sum + "," + count, "," + sumInWhile);
+		if (countItem != 0) {
+			sum += countItem;
 			console.log("Total number of rows crawled: " + (sum));
+		} else {
+			console.log(sum + "," + count);
 		}
 		start += 10;
 		stop = false;
@@ -114,24 +120,25 @@ async function asyncCall(req) {
 	// input key search
 	// let keySearch = "倉庫　軽作業　千葉県流山市";
 	const encSear = encodeURI(req.keyword);
-	let lrad = req.location != "2" ? ("&lrad=" + IRAD) : ("&lrad=2.0");
+	let lrad = req.location != "2" ? "&lrad=10.0" : "";
 	let chips = req.dayPosted != "0" ? ("&chips=date_posted:" + req.dayPosted + "&schips=date_posted;" + req.dayPosted) : ""
 	count = 10;
 	start = 0;
 	sum = 0;
 	dataRs = [];
 	listHashRecruitmentID = [];
+	sumInWhile = 0;
 	const dateNow = new Date();
 	// when data form gg < 10 record -> stop while
-	while(count > 0){
+	while(count > 0  && sumInWhile < 150){
 		if (!stop) {
 			stop = true;
 			const options = {
-				uri: 'https://www.google.com/search?yv=3&rciv=jb'+lrad+'&nfpr=0&q='+encSear+'&start='+start+'&asearch=jb_list&async=_id:VoQFxe,_pms:hts,_fmt:pc',
+				uri: 'https://www.google.com/search?yv=3&rciv=jb'+lrad+chips+'&nfpr=0&q='+encSear+'&start='+start+'&asearch=jb_list&async=_id:VoQFxe,_pms:hts,_fmt:pc',
 				headers: {
-					'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36',
+					'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
 					'sec-ch-ua-platform':  "Windows",
-					'accept-language':  `ja;q=0.9,fr;q=0.8,de;q=0.7,es;q=0.6,it;q=0.5,nl;q=0.4,sv;q=0.3,nb;q=0.2`
+					'accept-language':  `ja;q=0.9,fr;q=0.8,de;q=0.7,es;q=0.6,it;q=0.5,nl;q=0.4,sv;q=0.3,nb;q=0.2`,
 				},
 				json: true // Automatically parses the JSON string in the response
 			};
@@ -139,7 +146,7 @@ async function asyncCall(req) {
 			await contentMethod(options, req.bukken_id, dateNow, lrad);
 		}
 	}
-	if (sum == 0) console.log("data not found!")
+	if (sum == 0) console.log("data not found!");
 	else writeCsv(dataRs, req.keyword, dateNow);
 }
 
@@ -149,7 +156,7 @@ function writeCsv(jsonObject, keySearch, dateNow) {
 	const separator = ","
 	const fileType = "csv"
 	// example name: 倉庫 軽作業　千葉県流山市_2021-09-22_09-50-06_217.csv
-	const date = moment(dateNow).format('YYYY-MM-DD_hh-mm-ss-SSS');
+	const date = moment(dateNow).format('YYYY-MM-DD_HH-mm-ss-SSS');
 	const fileName = `${keySearch}_${date}_${sum}.${fileType}`;
 	const file = `${__dirname}/data/${fileName}`;
 	fileNameSuccess = fileName;
@@ -170,7 +177,11 @@ exports.asyncCall = async function(req){
     await asyncCall(req);
 	return {sum: sum, fileName: fileNameSuccess};
 }
-
+// asyncCall({bukken_id: "34",
+// dayPosted: "0",
+// keyword: "倉庫 軽作業　千葉県流山市",
+// location: "10.0",
+// no: "1"});
 
 
 
